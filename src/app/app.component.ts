cae17106dd345c40e6ff59c01f47c3388d82ba73
Mark Percy <mark.percy98@outlook.com>
2022-08-14 14:21:15 +0100

    Added ability to add tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent, TaskDialogResult } from './task-dialog/task-dialog.component';
 import { Task } from './task/task';
 
 @Component({
@@ -17,7 +19,26 @@ export class AppComponent {
   ]
   inProgress: Task[] = [];
   done: Task[] = [];
+  
+  constructor(private dialog: MatDialog) {
 
+  }
+  newTask(): void {
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '270px',
+      data: {
+        task:{},
+      },
+    });
+    dialogRef
+      .afterClosed()
+      .subscribe((result: TaskDialogResult|undefined) => {
+        if(!result) {
+          return;
+        }
+        this.todo.push(result.task)
+      })
+  }
   editTask(list: string, task: Task): void {}
 
   drop(event: CdkDragDrop<Task[]>):void {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,18 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { TaskComponent } from './task/task.component';
+import { TaskDialogComponent } from './task-dialog/task-dialog.component';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TaskComponent,
+    TaskDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +26,11 @@ import { TaskComponent } from './task/task.component';
     MatIconModule,
     MatToolbarModule,
     MatCardModule,
-    DragDropModule
+    DragDropModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
